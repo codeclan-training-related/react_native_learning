cb83f89329a9140f43435afc46743b8606900f87
zhu <zhumeng2224@163.com>
2023-07-04 22:15:41 +0100

    react native

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import {StyleSheet, Text, View, Button, TextInput } from 'react-native';
+// components exposed by react native
+
+// below is called functional components.
+// Text and View are built in components
+// view is to hold other components, instead of content
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+   <View style={styles.appContainer}>
+<View style={ styles.inputContainer}>
+  <TextInput placeholder="hello sunshine" />
+  <Button title='lalalalalal'/>
+</View>
+
+<View></View>
+
+
+   </View>
+
+     
+    </>
   );
 }
 
+// style prop is supported on view and text
+// StyleSheet is an object;  .create is a built in method:
 const styles = StyleSheet.create({
+  appContainer:{
+    padding: 100
+  },
+
+  inputContainer:{
+    flexDirection:"row",
+    justifyContent:"space-evenly"
+  },
+
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paragraph:{
+  color:'red'
+  }
 });
+
+
+
+// <View style={styles.container}>
+
+// <View>
+// {/* inline style below, but not recommended:  */}
+// <Text style={{color:'#ccc', margin: 16, borderWidth:2, borderColor:"pink", padding:16}}>Hello world!</Text>
+// </View>
+
+// <View>
+// <Text  style={styles.paragraph}> Test Test Three</Text>
+// </View>
+
+// <View style={styles.button}>
+// <Button title="income"/>
+// <Button title="outcome"/>
+// <Button title="balance"/>
+// </View>
+
+// </View>
